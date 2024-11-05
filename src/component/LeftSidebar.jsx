1409b4ae0bb2eb3a044351ea6c496fed7bb721bf
elrdan <seongo9655@gmail.com>
2024-11-05 10:01:04 +0900

    feat: 삭제버튼 추가

--- src/component/LeftSidebar.jsx
+++ src/component/LeftSidebar.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import {Button, Card, Divider} from "antd";
 
-
-const LeftSidebar = ({resetForm, savedEntries, loadSavedEntry}) => {
+const LeftSidebar = ({resetForm, savedEntries, loadSavedEntry, deleteEntry}) => {
     return (
-        <div style={{width: '15%', padding: '20px', borderRight: '1px solid #ddd'}}>
+        <>
             <Button type="primary" onClick={resetForm} style={{marginBottom: '10px'}}>
                 새로 만들기
             </Button>
@@ -12,17 +11,22 @@ const LeftSidebar = ({resetForm, savedEntries, loadSavedEntry}) => {
             <Divider>저장된 목록</Divider>
             {savedEntries.length > 0 ? (
                 savedEntries.map((entry, index) => (
-                    <Card key={index} onClick={() => loadSavedEntry(entry)}
-                          style={{marginBottom: '10px', cursor: 'pointer'}}>
-                        <p>상차지: {entry.loadLocation}</p>
-                        <p>하차지: {entry.unloadLocation}</p>
-                        <p>이사 종류: {entry.moveType?.value}</p>
+                    <Card key={index} style={{ display: 'flex', justifyContent: 'center', marginBottom: '10px', cursor: 'pointer'}}>
+                        <div onClick={() => loadSavedEntry(entry)}>
+                            <p>상차지: {entry.loadLocation}</p>
+                            <p>하차지: {entry.unloadLocation}</p>
+                            <p>이사 종류: {entry.moveType?.value}</p>
+                        </div>
+
+                        <Button type="danger" onClick={() => deleteEntry(entry.id)}>
+                            삭제
+                        </Button>
                     </Card>
                 ))
             ) : (
                 <p>저장된 정보가 없습니다.</p>
             )}
-        </div>
+        </>
     )
 }
 
