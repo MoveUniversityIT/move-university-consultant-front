f7799901cdaa58d3026948175e94ddbd1ecd0c7b
elrdan <seongo9655@gmail.com>
2024-12-04 07:18:41 +0900

    style: 화면 구성 비율 조정

--- src/appmain/components/Consultant.jsx
+++ src/appmain/components/Consultant.jsx
@@ -91,7 +91,7 @@ const Consultant = () => {
                 className="h-full grid gap-2 p-2 mx-auto overflow-x-auto"
                 style={{
                     marginRight: isCollapsed ? "0" : "650px",
-                    gridTemplateColumns: "minmax(240px, 1fr) minmax(550px, 2fr) minmax(400px, 3fr) minmax(190px, 1.5fr)",
+                    gridTemplateColumns: "minmax(240px, 1fr) minmax(550px, 2.5fr) minmax(400px, 2.5fr) minmax(190px, 1.5fr)",
                 }}
             >
                 <Reservation
